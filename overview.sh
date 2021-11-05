4ab97a5fb4d2dcdc16a10198cc3c1d1fb056fa7f
NuxRo <nux@li.nux.ro>
2021-11-05 09:23:11 +0000

    Update overview.sh

--- overview.sh
+++ overview.sh
@@ -9,21 +9,24 @@ export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 
 # please adjust the below with the correct MySQL credentials
 dbuser="root"
-dbpassword="password"
-db="cloud"
+dbpassword="P@ssword123"
+db="cloud_na"
 
 mysql_command="mysql -u$dbuser -p$dbpassword $db -N -s -r"
 
 #################################################################################
 
-echo "Showing zones:"
+echo -n "Cloudstack version: "; $mysql_command -e "select version from version where step='"Complete"' order by id desc limit 1;"
+echo "Management servers:"
+$mysql_command -e "select id,name,service_ip from mshost where removed is null;"
 echo
+echo "ZONES:"
 
 $mysql_command -e "select id,name from data_center where removed is null;"
 
 
 echo
-echo "Showing pods and clusters for each zone:"
+echo "Showing pods, clusters and hypervisors for each zone:"
 echo
 
 for zone in $($mysql_command -e "select id from data_center where removed is null;")
@@ -31,10 +34,11 @@ do
 echo
 echo "#################################"
 echo -n "ZONE: " ; $mysql_command -e "select name from data_center where id="$zone";"
-
+echo -n "SECURITY GROUPS: "; $mysql_command -e "select is_security_group_enabled from data_center where id="$zone";"
 echo "PRIMARY STORAGE (TiB): "
-echo -n "Total: "; $mysql_command -e "select sum(capacity_bytes)/1099511627776 from storage_pool where removed is null and data_center_id="$zone";"
-echo -n "Used:  "; $mysql_command -e "select sum(used_bytes)/1099511627776 from storage_pool where removed is null and data_center_id="$zone";"
+echo -n "- Total: "; $mysql_command -e "select sum(capacity_bytes)/1099511627776 from storage_pool where removed is null and data_center_id="$zone";"
+echo -n "- Used:  "; $mysql_command -e "select sum(used_bytes)/1099511627776 from storage_pool where removed is null and data_center_id="$zone";"
+echo "--------------------------------"
 for podid in $( $mysql_command -e "select id from host_pod_ref where removed is null and data_center_id="$zone";" ); do
 echo -n "- POD: "; $mysql_command -e "select name from host_pod_ref where id="$podid";"
 #$mysql_command -e "select name from cluster where removed is null and data_center_id="$zone" and pod_id="$podid";"
@@ -42,7 +46,7 @@ for clusterid in $($mysql_command -e "select id from cluster where removed is nu
 echo -n "-- CLUSTER: "
 $mysql_command -e "select name from cluster where removed is null and data_center_id="$zone" and pod_id="$podid" and id="$clusterid";"
 echo "--- HYPERVISORS:"
-$mysql_command -e "select name from host where type='"ROUTING"' and cluster_id="$clusterid";"
+$mysql_command -e "select name,private_ip_address,storage_ip_address,public_ip_address from host where type='"ROUTING"' and cluster_id="$clusterid";"
 done
 done
 echo "#################################"
